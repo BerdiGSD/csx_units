66c9d985a419fe10f185f35c2dcaf1333ccf48ba
Ken <kennethrberdiel1@outlook.com>
2023-08-05 14:13:59 -0500

    Had to make a few corrections to for how the files were organized.

---
+++ Functions and Execution Context/Last Letter/lastLetter.js
@@ -0,0 +1,12 @@
+function lastLetter(word) {
+    // INSERT CODE HERE
+    let final = word[word.length-1];
+    
+    return final;
+    }
+
+  // Uncomment the lines below to test your code
+  console.log(lastLetter("hello")); //=> "o"
+  console.log(lastLetter("goodbye!")); //=> "!"
+  console.log(lastLetter("ZeltoiD")); //=> "D"
+  console.log(lastLetter("I love Javascript")); //=> "t"
